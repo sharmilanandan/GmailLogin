81b2c32cebab8815067a212533281b13eb26de2a
sharmilanandan <34845267+sharmilanandan@users.noreply.github.com>
2018-03-23 19:53:32 +0530

    modified

--- src/delete/Test1.java
+++ src/delete/Test1.java
@@ -1,17 +1,18 @@
 package delete;
 
-import generic.BaseTest;
+
+import generic.BaseTest1;
 import generic.Excel;
 
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-public class Test1 extends BaseTest {
+public class Test1 extends BaseTest1 {
 	@Test
 	public void openApp(){
 		String s=driver.getCurrentUrl();
 		Reporter.log(s,true);
-		Excel.writeDataToExcel("./data/input.xlsx", "Sheet1");
+		//Excel.writeDataToExcel("./data/input.xlsx", "Sheet1");
 	}
 	
 
